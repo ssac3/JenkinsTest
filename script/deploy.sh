886377aaef31de38230a070566668d13834a3b1c
DESKTOP-7DI33P4\gktjd <039818>
2022-06-11 15:40:13 +0900

    no message

--- script/deploy.sh
+++ script/deploy.sh
@@ -13,19 +13,4 @@ kill -9 $CURRENT_PID
 sleep 10
 fi
  echo ">어플리케이션 배포 진행!"
-nohup java -jar /home/ec2-user/deploy/dokotlin-0.0.1-SNAPSHOT.jar >> /home/ec2-user/deploy/logs/dokotlin.log &!#/bin/bash
-
-                                                                                                              echo "Start Spring Boot Application!"
-                                                                                                              CURRENT_PID=$(ps -ef | grep java | grep dokotlin | awk '{print $2}')
-                                                                                                              echo "$CURRENT_PID"
-
-                                                                                                               if [ -z $CURRENT_PID ]; then
-                                                                                                              echo ">현재 구동중인 어플리케이션이 없으므로 종료하지 않습니다."
-
-                                                                                                              else
-                                                                                                              echo "> kill -9 $CURRENT_PID"
-                                                                                                              kill -9 $CURRENT_PID
-                                                                                                              sleep 10
-                                                                                                              fi
-                                                                                                               echo ">어플리케이션 배포 진행!"
-                                                                                                              nohup java -jar /home/ec2-user/deploy/dokotlin-0.0.1-SNAPSHOT.jar >> /home/ec2-user/deploy/logs/dokotlin.log &
+nohup java -jar /var/lib/jenkins/workspace/server/build/libs/server-0.0.1-SNAPSHOT.jar >> /home/ec2-user/deploy/logs/dokotlin.log &!#/bin/bash
