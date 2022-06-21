e9bebda1469dd87a8a64af39c7c2061ead68f54e
chaeyeon <pcyeon07@gmail.com>
2022-06-21 22:10:13 +0900

    feat: #11 부서별 근무시간 수정

--- src/main/java/com/example/server/model/dao/manager/DepartmentMapper.java
+++ src/main/java/com/example/server/model/dao/manager/DepartmentMapper.java
@@ -4,6 +4,9 @@ import com.example.server.model.dto.manager.Department;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Repository
 @Mapper
 public interface DepartmentMapper {
@@ -11,7 +14,7 @@ public interface DepartmentMapper {
     int existDeptId(Long id);
     Department findByDeptInfo(Long id);
 
-    void updateByOne(String id, String start_time, String end_time);
+    void updateByOne(LocalDateTime startTime, LocalDateTime endTime, Long id);
 
     void save(Department department);
 }
--- src/main/java/com/example/server/service/DepartmentService.java
+++ src/main/java/com/example/server/service/DepartmentService.java
@@ -4,27 +4,25 @@ import com.example.server.constants.JsonResponse;
 import com.example.server.constants.StatusCode;
 import com.example.server.model.dao.manager.DepartmentMapper;
 import com.example.server.model.dto.manager.Department;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
 public class DepartmentService {
     private final DepartmentMapper departmentMapper;
-
+    private StatusCode statusCode;
     @Transactional
     public ResponseEntity<StatusCode> findByOne(Long id) {
-        StatusCode statusCode;
         int result = departmentMapper.existDeptId(id);
 
         if(result == 0){
-            statusCode = StatusCode.builder().resCode(1).resMsg("존재하지 않는 부서ID입니다.").build();
+            statusCode = StatusCode.builder().resCode(1).resMsg("존재하지 않는 부서 ID 입니다.").build();
         }else{
             Department department = departmentMapper.findByDeptInfo(id);
             statusCode = StatusCode.builder().resCode(0).resMsg("부서 정보 조회 성공").data(department).build();
@@ -33,7 +31,17 @@ public class DepartmentService {
     }
 
     @Transactional
-    public ResponseEntity<StatusCode> updateByOne(Long id, String start, String end){
-        return null;
+    public ResponseEntity<StatusCode> updateByOne(Long id, String startTime, String endTime){
+        int checkValidId = departmentMapper.existDeptId(id);
+
+        if(checkValidId == 0){
+            statusCode = StatusCode.builder().resCode(1).resMsg("존재하지 않는 부서 ID 입니다.").build();
+        }else{
+            LocalDateTime sDate = LocalDateTime.parse(startTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            LocalDateTime eDate = LocalDateTime.parse(endTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            departmentMapper.updateByOne(sDate, eDate, id);
+            statusCode = StatusCode.builder().resCode(0).resMsg("출/퇴근 시간 수정 완료").build();
+        }
+        return new JsonResponse().send(HttpStatus.OK, statusCode);
     }
 }
