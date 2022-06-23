7106d311ecb9fda42e934922713808f3a6760f45
DESKTOP-7DI33P4\gktjd <039818>
2022-06-23 23:11:45 +0900

    refactor: request -> principalDetails 수정

--- src/main/java/com/example/server/service/AttendanceService.java
+++ src/main/java/com/example/server/service/AttendanceService.java
@@ -1,5 +1,6 @@
 package com.example.server.service;
 
+import com.example.server.config.auth.PrincipalDetails;
 import com.example.server.constants.JsonResponse;
 import com.example.server.constants.StatusCode;
 import com.example.server.model.dao.user.AttendanceMapper;
@@ -11,20 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AttendanceService {
-
     private final AttendanceMapper attendanceMapper;
-    StatusCode statusCode;
 
-    public ResponseEntity<StatusCode> getAllAttendance(HttpServletRequest request) {
-        String username = request.getAttribute("username").toString();
-        List<MonthJoin> monthJoin = attendanceMapper.getAllAttendance(Long.parseLong(username));
-        statusCode = StatusCode.builder().resCode(0).resMsg("근태 정보 조회 완료").data(monthJoin).build();
-        return new JsonResponse().send(HttpStatus.OK, statusCode);
+    public ResponseEntity<StatusCode> getAllAttendance(String username) {
+        return Optional.of(new JsonResponse())
+                .map(v -> {
+                    List<MonthJoin> monthJoin = attendanceMapper.getAllAttendance(Long.parseLong(username));
+                    return new JsonResponse().send(HttpStatus.OK, StatusCode.builder().resCode(0).resMsg("근태 정보 조회 완료").data(monthJoin).build());
+                }).orElseGet(() -> new JsonResponse().send(HttpStatus.BAD_REQUEST, StatusCode.builder().resCode(0).resMsg("근태 정보 조회 완료").build()));
     }
-
-
 }
