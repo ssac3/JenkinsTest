15ece7a1a1a2c49ab5e5b52482b9eb7ca5aadd6d
DESKTOP-7DI33P4\gktjd <039818>
2022-06-24 11:03:52 +0900

    refactor: 미사용 코드 제거

--- src/main/java/com/example/server/service/VacationService.java
+++ src/main/java/com/example/server/service/VacationService.java
@@ -13,8 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
