cd9ef2986218432cc98c0f0eaa5b506548fcb9eb
yunji <lshswordd8@gmail.com>
2022-07-08 08:59:42 +0900

    feat #73 사원사진등록

--- src/main/java/com/example/server/controller/AdminController.java
+++ src/main/java/com/example/server/controller/AdminController.java
@@ -8,8 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.amazonaws.services.cloudsearchv2.model.IndexFieldType.Date;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,10 +26,22 @@ public class AdminController {
         return adminService.viewEmp(principalDetails.getUsername(), user);
     }
 
+    // 사원번호 생성
+//    @PostMapping("mkUsername")
+//    public ResponseEntity<StatusCode> mkUsername(@AuthenticationPrincipal PrincipalDetails principalDetails, User user){
+//        System.out.println("mkUsername");
+//        int year = new Date().get
+//        return ;
+//    }
+
     //사원등록
     @PostMapping("/admin/insertEmp")
-    public ResponseEntity<StatusCode> insertEmp(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody User user){
-        return adminService.insertEmp(principalDetails.getUsername(), user);
+//    public ResponseEntity<StatusCode> insertEmp(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody User user){
+//        return adminService.insertEmp(principalDetails.getUsername(), user); , @RequestPart(value = "data") User user
+    public ResponseEntity<StatusCode> insertEmp(@RequestPart(value = "image") MultipartFile multipartFile, @RequestPart(value = "data") User user){
+        System.out.println("user는"+user);
+        System.out.println("쳌쳌" + multipartFile);
+        return adminService.insertEmp(multipartFile, "profile", user);
     }
 
     //사원삭제
--- src/main/java/com/example/server/service/AdminService.java
+++ src/main/java/com/example/server/service/AdminService.java
@@ -1,17 +1,27 @@
 package com.example.server.service;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.dynamodbv2.xspec.S;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.server.constants.JsonResponse;
 import com.example.server.constants.StatusCode;
 import com.example.server.model.dao.admin.AdminMapper;
 import com.example.server.model.dto.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -22,20 +32,64 @@ public class AdminService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AdminMapper adminMapper;
     private StatusCode statusCode;
+    private final AmazonS3Client amazonS3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    // 사원번호 생성
 
     //사원등록
     @Transactional
-    public ResponseEntity<StatusCode> insertEmp(String userInfo, User user){
-       //username 유효성겁사(중복)
-        if(userInfo != null && !userInfo.equals("")){
-            adminMapper.insertEmp(user.toInsertEntity(bCryptPasswordEncoder));
-            statusCode = StatusCode.builder().resCode(0).resMsg("사원등록을 성공했습니다").build();
-        }else {
-            System.out.println("[ERR] 유효하지 않는 사용자 정보입니다.");
-            statusCode = StatusCode.builder().resCode(2).resMsg("유효하지 않는 사용자 정보입니다.").build();
-        }
+//    public ResponseEntity<StatusCode> insertEmp(MultipartFile multipartFile, User user){
+    public ResponseEntity<StatusCode> insertEmp(MultipartFile multipartFile, String dirName, User user){
+        System.out.println("multipartFile = " + multipartFile);
+        System.out.println("user.getUsername() = " + user.getUsername());
+
+        String fileUrl = dirName +"/"+ user.getUsername() +"_"+multipartFile.getOriginalFilename();
+        System.out.println("fileUrl = " + fileUrl);
+        String uploadImageUrl = putS3(multipartFile, fileUrl, dirName); //s3 upload
+        // upload method end
+        String awsUrl = uploadImageUrl;
+        String insertUrl = awsUrl + "/" +user.getUsername() + "_" + multipartFile.getOriginalFilename();
+//        adminMapper.insertEmp(user.toInsertEntity(bCryptPasswordEncoder));
+        adminMapper.insertEmp(user.toInsertEntity(bCryptPasswordEncoder, insertUrl));
+        statusCode = StatusCode.builder().resCode(0).resMsg("사원등록을 성공했습니다").build();
         return new JsonResponse().send(HttpStatus.OK, statusCode);
+
+
+        // username 유효성겁사(중복)
+
+//        if(userInfo != null && !userInfo.equals("")){
+//            System.out.println("ㅗㅗ");
+//            // 업로드 시작
+//            // 파일이름
+////            String fileUrl = dirName +"/"+ username +"_"+multipartFile.getOriginalFilename();
+//            String fileUrl = dirName +"/"+ user.getUsername() +"_"+multipartFile.getOriginalFilename();
+//            System.out.println("fileUrl = " + fileUrl);
+//            String uploadImageUrl = putS3(multipartFile, fileUrl, dirName); //s3 upload
+//            String insertUrl = uploadImageUrl+ "/" +user.getUsername() + "_" + multipartFile.getOriginalFilename();
+//            // 끝 업로드
+////            adminMapper.insertEmp(user.toInsertEntity(bCryptPasswordEncoder));
+//            adminMapper.insertEmp(user.toInsertEntity(bCryptPasswordEncoder));
+//            statusCode = StatusCode.builder().resCode(0).resMsg("사원등록을 성공했습니다").build();
+//        }else {
+//            System.out.println("[ERR] 유효하지 않는 사용자 정보입니다.");
+//            statusCode = StatusCode.builder().resCode(2).resMsg("유효하지 않는 사용자 정보입니다.").build();
+//        }
+
     }
+//    @Transactional
+//    public ResponseEntity<StatusCode> insertEmp(String userInfo, User user){
+//        //username 유효성겁사(중복)
+//        if(userInfo != null && !userInfo.equals("")){
+//            adminMapper.insertEmp(user.toInsertEntity(bCryptPasswordEncoder));
+//            statusCode = StatusCode.builder().resCode(0).resMsg("사원등록을 성공했습니다").build();
+//        }else {
+//            System.out.println("[ERR] 유효하지 않는 사용자 정보입니다.");
+//            statusCode = StatusCode.builder().resCode(2).resMsg("유효하지 않는 사용자 정보입니다.").build();
+//        }
+//        return new JsonResponse().send(HttpStatus.OK, statusCode);
+//    }
 
     // 사원리스트정보
     @Transactional
@@ -67,7 +121,7 @@ public class AdminService {
         }
         return new JsonResponse().send(HttpStatus.OK, statusCode);
     }
-    
+
     //사원삭제
     @Transactional
     public ResponseEntity<StatusCode> deleteEmp(String userInfo, User user) {
@@ -90,7 +144,7 @@ public class AdminService {
             adminMapper.updateEmp(User.builder().
                     name(user.getName())
                     .gender(user.getGender())
-//                    .img(user.getImg())
+                    .img(user.getImg())
                     .depId(user.getDepId())
                     .position(user.getPosition())
                     .role(user.getRole())
@@ -108,5 +162,50 @@ public class AdminService {
         }
         return new JsonResponse().send(HttpStatus.OK, statusCode);
     }
+
+    // S3
+    public String upload(MultipartFile multipartFile, String dirName, String username){
+        String fileUrl = dirName +"/"+ username +"_"+multipartFile.getOriginalFilename(); // S3에 저장될 파일 이름
+        System.out.println("fileUrl = " + fileUrl);
+        String uploadImageUrl = putS3(multipartFile, fileUrl, dirName); //s3 upload
+        return uploadImageUrl;
+    }
+
+    public String putS3(MultipartFile multipartFile, String fileName, String dirName){
+        try {
+            String contentType = multipartFile.getContentType();
+            long contentLength = multipartFile.getSize();
+
+            InputStream is = multipartFile.getInputStream();
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(contentType);
+            objectMetadata.setContentLength(contentLength);
+
+            amazonS3Client.putObject(new PutObjectRequest(this.bucket, fileName, is, objectMetadata));
+            System.out.println(String.format("[%s] upload complete", fileName));
+        }catch (AmazonS3Exception e){
+            e.getMessage();
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return amazonS3Client.getUrl(bucket, dirName).toString();
+    }
+    /*public void remove(String fileName) {
+        try {
+            System.out.println("fileName : "+fileName);
+            //Delete 객체 생성
+            DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucket, fileName);
+            //Delete
+            amazonS3Client.deleteObject(deleteObjectRequest);
+            System.out.println(String.format("[%s] delete complete", fileName));
+        } catch (AmazonServiceException e) {
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            e.printStackTrace();
+        }
+    }*/
 }
 
